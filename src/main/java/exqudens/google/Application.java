a93ccfa08ef9d75b5702dfdd98d68c44bc6a78cf
exqudens <exqudens@gmail.com>
2023-10-03 22:31:39 -0700

    step 3

--- src/main/java/exqudens/google/Application.java
+++ src/main/java/exqudens/google/Application.java
@@ -19,7 +19,11 @@ import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.StringReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @FieldDefaults(level = AccessLevel.PRIVATE)
 @Slf4j
@@ -29,61 +33,93 @@ public class Application {
 
     public static void run(String... args) {
         try {
-            System.out.println("-".repeat(10));
+            String jsonPath = null;
+            String tokensPath = "tmp";
+
             for (String arg : args) {
-                System.out.println(arg);
+                log.info("arg: '" + arg + "'");
             }
-            System.out.println("-".repeat(10));
-            /*
-            // Global instance of the JSON factory.
-            JsonFactory jsonFactory = GsonFactory.getDefaultInstance();
 
-            // Build a new authorized API client service.
-            NetHttpTransport netHttpTransport = GoogleNetHttpTransport.newTrustedTransport();
-            List<String> scopes = List.of(DriveScopes.DRIVE_METADATA_READONLY);
-            Credential credential = getCredentials(jsonFactory, netHttpTransport, scopes);
-            Drive service = new Drive.Builder(netHttpTransport, jsonFactory, credential)
-                    .setApplicationName(APPLICATION_NAME)
-                    .build();
+            if (args.length > 0) {
+                jsonPath = args[0];
+            }
 
-            // Print the names and IDs for up to 10 files.
-            FileList result = service.files().list()
-                    .setPageSize(10)
-                    .setFields("nextPageToken, files(id, name)")
-                    .execute();
-            List<File> files = result.getFiles();
-            if (files == null || files.isEmpty()) {
-                System.out.println("No files found.");
+            if (args.length > 1) {
+                tokensPath = args[1];
+            }
+
+            log.info("jsonPath: {}'",  jsonPath);
+            log.info("tokensPath: '{}'", tokensPath);
+
+            List<String> results = listFiles(jsonPath, tokensPath);
+            if (results.isEmpty()) {
+                System.out.println("<NO_FILES_FOUND>");
             } else {
-                System.out.println("Files:");
-                for (File file : files) {
-                    System.out.printf("%s (%s)\n", file.getName(), file.getId());
+                for (String result : results) {
+                    System.out.println(result);
                 }
             }
-            */
         } catch (Exception e) {
             log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
         }
     }
 
+    private static List<String> listFiles(
+            final String jsonPath,
+            final String tokensPath
+    ) throws Exception {
+        Objects.requireNonNull(jsonPath);
+        List<String> results = new ArrayList<>();
+
+        // Read json file content.
+        String json = Files.readString(Path.of(jsonPath));
+
+        // Global instance of the JSON factory.
+        JsonFactory jsonFactory = GsonFactory.getDefaultInstance();
+
+        // Build a new authorized API client service.
+        NetHttpTransport netHttpTransport = GoogleNetHttpTransport.newTrustedTransport();
+        List<String> scopes = List.of(DriveScopes.DRIVE_METADATA_READONLY);
+        Credential credential = getCredentials(json, jsonFactory, netHttpTransport, scopes, tokensPath);
+        Drive service = new Drive.Builder(netHttpTransport, jsonFactory, credential)
+                .setApplicationName(APPLICATION_NAME)
+                .build();
+
+        // Print the names and IDs for up to 10 files.
+        FileList result = service.files().list()
+                .setPageSize(10)
+                .setFields("nextPageToken, files(id, name)")
+                .execute();
+        List<File> files = result.getFiles();
+        if (files != null && !files.isEmpty()) {
+            for (File file : files) {
+                results.add(file.getName() + " (" + file.getId() + ")");
+            }
+        }
+        return results;
+    }
+
     private static Credential getCredentials(
+            final String json,
             final JsonFactory jsonFactory,
             final NetHttpTransport netHttpTransport,
-            final List<String> scopes
+            final List<String> scopes,
+            final String tokensDirectoryPath
     ) throws Exception {
         Credential credential;
-        String json = "";
 
         // Load client secrets.
         GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(jsonFactory, new StringReader(json));
 
         // Build flow and trigger user authorization request.
         GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(netHttpTransport, jsonFactory, clientSecrets, scopes)
-                .setDataStoreFactory(new FileDataStoreFactory(new java.io.File("tokens")))
+                .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(tokensDirectoryPath)))
                 .setAccessType("offline")
                 .build();
         LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
         credential = new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
+
         return credential;
     }
 
